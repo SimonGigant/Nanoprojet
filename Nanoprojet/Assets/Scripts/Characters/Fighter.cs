253ee0d6f5f147587981d83fe7147d24cb9aae6f
Simon <666ohyeah999@gmail.com>
2019-11-14 15:15:20 +0100

    :construction: :sparkles: Basic control, movement and dash
    Using old input system

---
+++ Nanoprojet/Assets/Scripts/Characters/Fighter.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FighterState {Idle, SetUpAttack, Block, Parry, Attack, AttackLag, Dash, Hit, Throw, Death};
+
+public class Fighter : MonoBehaviour
+{
+    //Stats
+    private float speed = 0.1f;
+    private int maxHP = 3;
+    
+    private float dashSpeed = 0.3f;
+    private float dashDuration = 0.5f;
+    private float setUpAttackDuration;
+    private float blockDuration;
+    private float attackDuration;
+    private float attackLagDuration;
+
+    //Values
+    private int hp;
+    private FighterState state;
+    private Vector2 currentDirection;
+
+    //Counters
+    private float counterInState;
+
+    [SerializeField] private Fighter opponent;
+
+    private void Initialize()
+    {
+        hp = maxHP;
+        state = FighterState.Idle;
+    }
+
+    private void FaceOpponent()
+    {
+        transform.LookAt(opponent.transform, transform.forward);
+    }
+    
+    void Start()
+    {
+        Initialize();
+    }
+    
+    void Update()
+    {
+        switch (state)
+        {
+            case FighterState.Idle:
+                {
+                    Idle();
+                    break;
+                }
+            case FighterState.Dash:
+                {
+                    Dash();
+                    break;
+                }
+        }
+    }
+
+    private void ChangeState(FighterState nextState)
+    {
+        //Initialisations
+        counterInState = 0f;
+        switch (nextState)
+        {
+            case FighterState.Dash:
+                {
+
+                    break;
+                }
+        }
+        state = nextState;
+    }
+
+    private void Idle()
+    {
+        FaceOpponent();
+    }
+
+    private void Dash()
+    {
+        counterInState += Time.deltaTime;
+        Movement(currentDirection * dashSpeed);
+        if (counterInState > dashDuration)
+        {
+            ChangeState(FighterState.Idle);
+        }
+    }
+
+    private void Movement(Vector2 movement)
+    {
+        transform.position += new Vector3(movement.x, 0f, movement.y);
+    }
+
+    //************************************************************************
+    //Public methods to control the character in Control class and derivatives
+    //************************************************************************
+
+    public bool Move(Vector2 dir)
+    {
+        if(dir.magnitude.Equals(0f))
+        {
+            return false;
+        }
+        currentDirection = dir.normalized;
+        if (state == FighterState.Dash)
+        {
+            return true;
+        }
+        //We ensure magnitude is <= 1f not to move faster than the character speed
+        if (dir.magnitude > 1f)
+        {
+            dir.Normalize();
+        }
+        Vector2 movement = dir * speed;
+        Movement(movement);
+        return true;
+    }
+
+    public bool DashButton()
+    {
+        if (state == FighterState.Idle)
+        {
+            ChangeState(FighterState.Dash);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Nanoprojet/Assets/Scripts/Control/Human.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : Control
+{
+    [SerializeField] private Fighter fighter;
+    [SerializeField] private int controller;
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                fighter.DashButton();
+            }
+            if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
+            {
+                fighter.Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+            }
+        }
+    }
+}
