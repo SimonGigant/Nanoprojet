af7bdd7fa6f1749d84ff14b852e750a4bca8e33e
Nicolas <n.leray94@gmail.com>
2019-12-03 18:43:22 +0100

    :construction: light optimization + post process test

---
+++ Nanoprojet/Assets/Scripts/Characters/StickFX.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+[RequireComponent(typeof(Fighter))]
+public class StickFX : MonoBehaviour
+{
+	[SerializeField]
+	private VisualEffect vfx;
+	private Fighter fighter;
+
+	
+	public FXState defaultState;
+	public FXState setUpAttackState;
+	public FXState blockState;
+	public FXState attackState;
+	public FXState attackLagState;
+
+
+	public class FXState
+	{
+		[GradientUsage(true)]
+		public Gradient gradient;
+		public float intensity;
+
+		public void Apply(VisualEffect fx)
+		{
+			fx.SetFloat("Intensity", intensity);
+			fx.SetGradient("MainGradient", gradient);
+		}
+	}
+
+
+	private void Awake()
+	{
+		fighter = GetComponent<Fighter>();
+	}
+
+	private void Update()
+	{
+		UpdateFX();
+	}
+
+
+
+	private void UpdateFX()
+	{
+		switch (fighter.currentState)
+		{
+			case FighterState.SetUpAttack:
+				setUpAttackState.Apply(vfx);
+				break;
+			case FighterState.Block:
+				blockState.Apply(vfx);
+				break;
+			case FighterState.Attack:
+				attackState.Apply(vfx);
+				break;
+			case FighterState.AttackLag:
+				attackLagState.Apply(vfx);
+				break;
+			default:
+				defaultState.Apply(vfx);
+				break;
+		}
+	}
+}
